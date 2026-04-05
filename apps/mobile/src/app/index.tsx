ca08a0c383d56b86f374446f5c6fbe340304f86d
masch <the.masch@gmail.com>
2026-04-04 22:45:27 -0300

    feat(mobile): add tabs navigation and redirect home to projects

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,12 +1,5 @@
-import { View } from "react-native";
-import { Link } from "expo-router";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View className="flex-1 bg-gray-50 items-center justify-center px-5 pt-20">
-      <Link href="/projects" className="text-green-500">
-        Projects
-      </Link>
-    </View>
-  );
+  return <Redirect href="/project" />;
 }
