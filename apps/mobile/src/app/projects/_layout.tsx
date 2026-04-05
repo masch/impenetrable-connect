53163cbeaa2ed33fb0a188ddf519489c35d95bb0
Mario Chacon <53308354+masch@users.noreply.github.com>
2026-04-04 22:47:02 -0300

    feat(mobile): add tabs navigation for screens (#35)
    * feat(mobile): add tabs navigation with projects tab
    
    * feat(mobile): add tabs navigation and redirect home to projects

--- apps/mobile/src/app/projects/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Stack } from "expo-router";
-import { DEFAULT_SCREEN_OPTIONS } from "../../constants/nav.constants";
-
-export default function ProjectsLayout() {
-  return <Stack screenOptions={DEFAULT_SCREEN_OPTIONS} />;
-}
